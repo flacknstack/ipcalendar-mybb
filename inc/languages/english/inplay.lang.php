7e3e0418ea7fef0e63a352359628339ecf6a612a
Stack <gambill.staci@gmail.com>
2026-03-17 19:58:41 -0400

    Create inplay.lang.php

---
+++ inc/languages/english/inplay.lang.php
@@ -0,0 +1,67 @@
+<?php
+// general
+$l['inplaykalender'] = "Inplaykalender";
+$l['inplaykalender_monat'] = "Monat";
+$l['inplaykalender_jahr'] = "Jahr";
+$l['inplaykalender_add'] = "Event hinzuf&uuml;gen";
+$l['inplaykalender_events'] = "Inplay-Events";
+
+// klassen
+$l['inplaykalender_class_scenes'] = "szenen";
+$l['inplaykalender_class_birthday'] = "geburtstag";
+$l['inplaykalender_class_timeline'] = "timeline";
+$l['inplaykalender_class_event'] = "event";
+
+// monate
+$l['inplaykalender_januar'] = "Januar";
+$l['inplaykalender_februar'] = "Februar";
+$l['inplaykalender_maerz'] = "Maerz";
+$l['inplaykalender_april'] = "April";
+$l['inplaykalender_mai'] = "Mai";
+$l['inplaykalender_juni'] = "Juni";
+$l['inplaykalender_juli'] = "Juli";
+$l['inplaykalender_august'] = "August";
+$l['inplaykalender_september'] = "September";
+$l['inplaykalender_oktober'] = "Oktober";
+$l['inplaykalender_november'] = "November";
+$l['inplaykalender_dezember'] = "Dezember";
+
+$l['inplaykalender_januar_en'] = "January";
+$l['inplaykalender_februar_en'] = "February";
+$l['inplaykalender_maerz_en'] = "March";
+$l['inplaykalender_april_en'] = "April";
+$l['inplaykalender_mai_en'] = "May";
+$l['inplaykalender_juni_en'] = "June";
+$l['inplaykalender_juli_en'] = "July";
+$l['inplaykalender_august_en'] = "August";
+$l['inplaykalender_september_en'] = "September";
+$l['inplaykalender_oktober_en'] = "October";
+$l['inplaykalender_november_en'] = "November";
+$l['inplaykalender_dezember_en'] = "December";
+
+// tage
+$l['inplaykalender_montag'] = "Montag";
+$l['inplaykalender_dienstag'] = "Dienstag";
+$l['inplaykalender_mittwoch'] = "Mittwoch";
+$l['inplaykalender_donnerstag'] = "Donnerstag";
+$l['inplaykalender_freitag'] = "Freitag";
+$l['inplaykalender_samstag'] = "Samstag";
+$l['inplaykalender_sonntag'] = "Sonntag";
+
+// event hinzuf�gen
+$l['inplaykalender_event_name'] = "Name des Events";
+$l['inplaykalender_event_date'] = "Datum";
+$l['inplaykalender_event_date_start'] = "Startdatum";
+$l['inplaykalender_event_date_end'] = "Enddatum";
+$l['inplaykalender_event_desc'] = "Beschreibung";
+$l['inplaykalender_added'] = "Dein Event wurde hinzugef&uuml;gt!";
+
+// thread an event kn�pfen
+$l['inplaykalender_event_thread'] = "Verkn&uuml;pftes Event";
+$l['inplaykalender_event_thread_desc'] = "Zu welchem Event geh&ouml;rt diese Szene?";
+$l['inplaykalender_event_thread_select'] = "Event ausw&auml;hlen";
+
+$l['inplaykalender_scenes'] = "Inplayszenen";
+$l['inplaykalender_birthday'] = "Geburtstage";
+$l['inplaykalender_timeline'] = "Chronik-Events";
+ ?>
